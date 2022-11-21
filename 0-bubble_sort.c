4ce9ff7e1ac3afceabc501c2a1e459bbaba070ba
Ian Ewdin Kitembe <ianedwin@outlook.com>
2022-11-21 18:14:52 +0300

    repo name

---
+++ 0-bubble_sort.c
@@ -0,0 +1,45 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - sorts an array of ints in ascending order using Bubble Sort
+ * @array: array of ints to be sorted
+ * @size: size of array
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, len = size;
+	bool bubble_up = false;
+
+	if (array == NULL || size < 2)
+		return;
+
+	while (bubble_up == false)
+	{
+		bubble_up = true;
+		for (i = 0; i < len - 1; i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				swap_ints(array + i, array + i + 1);
+				print_array(array, size);
+				sleep (2);
+				bubble_up = false;
+			}
+		}
+		len--;
+	}
+}
+
+/**
+ * swap_ints - swap two ints in an array
+ * @a: first int
+ * @b: second int
+ */
+void swap_ints(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
---
+++ README.md
@@ -0,0 +1,11 @@
+# 0x1B. C - Sorting algorithms & Big O
+# C
+# Algorithm
+# Data structure
+ `By`: `Alexandre Gautier`
+ `Weight`: `2`
+ Project to be done in teams of not more than 2 people (your team: Ian Kitembe)
+ Project over - took place from Nov 2, 2022 6:00 AM to Nov 9, 2022 6:00 AM
+ `An auto review will be launched at the deadline`
+
+
